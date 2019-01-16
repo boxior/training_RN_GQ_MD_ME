6fb04c21d27ab853283176764490a8c398bcd996
Serhii <serhii.lyzun@limestonedigital.co>
2019-01-16 21:30:46 +0300

    add facebookLogin component

---
+++ client/src/authorization/facebook.js
@@ -0,0 +1,18 @@
+import FacebookLogin from 'react-facebook-login';
+import React, {Component} from "react";
+import {Platform, StyleSheet, Text, View, YellowBox} from 'react-native';
+//styled
+
+export default class FacebookAuth extends Component {
+    static propTypes = {};
+
+    static defaultProps = {};
+
+    render() {
+        return (
+            <View>
+                <Text>FFFFFFFFFFFFFFFFFFF</Text>
+            </View>
+        );
+    }
+}
